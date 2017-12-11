5871da368310d0779e77e208236fca8590f0d75f
bizhou.liu <bizhou.liu@chinaredstar.com>
2017-12-11 14:24:00 +0800

    Revert: update

---
+++ p-contract-h5/src/pages/process/toPage.vue
@@ -0,0 +1,33 @@
+<style type="text/scss" lang="scss" scoped>
+
+</style>
+
+<template>
+  <div>跳转中。。。</div>
+</template>
+
+<script>
+  import Jump from '../../assets/js/jump';
+
+  export default {
+    data() {
+      return {};
+    },
+    methods: {},
+    created() {
+      const {name} = this.$route;
+      const {
+        procInstId, serialNumber, procCode, sn
+      } = this.$route.query;
+      const query = {
+        procInstId,
+        serialNumber: serialNumber || '',
+        sn,
+        procCode,
+        routeName: name,
+        from: 'out'
+      };
+      new Jump(this.$router).init(query);
+    }
+  };
+</script>
