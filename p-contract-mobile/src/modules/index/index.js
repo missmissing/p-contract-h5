be8f38ad7b426d6ec3cc6fd434e98ce354c2739b
bizhou.liu <bizhou.liu@chinaredstar.com>
2017-12-28 18:51:28 +0800

    隐藏龙眼头部

--- p-contract-mobile/src/modules/index/index.js
+++ p-contract-mobile/src/modules/index/index.js
@@ -10,51 +10,47 @@ import getRouter from './router';
 import queryString from '../../util/queryString';
 import Api from '../../api';
 import Jump from './util/jump';
-import loadScripts from './util/loadScripts';
 import '../../assets/css/common.scss';
 
 Vue.use(Router);
 Vue.use(Vuex);
 component(Vue);
 
-loadScripts(() => {
-  window._____processCenterPageAction('setpageloadedheaderhide');
+const router = getRouter(Router);
+const store = vueStore(Vuex);
 
-  const router = getRouter(Router);
-  const store = vueStore(Vuex);
-
-  new Vue({
-    el: '#app',
-    mixins: [errorHanding],
-    router,
-    store,
-    components: {App},
-    data() {
-      return {
-        canRender: false
-      };
-    },
-    beforeCreate() {
-      const query = queryString();
-      const {employeecode} = query;
-      Api.login({userId: employeecode}).then((res) => {
-        const data = res.data.dataMap;
-        const {userInfo} = data;
-        LocalStore.set('user', userInfo);
+new Vue({
+  el: '#app',
+  mixins: [errorHanding],
+  router,
+  store,
+  components: {App},
+  data() {
+    return {
+      canRender: false
+    };
+  },
+  beforeCreate() {
+    const query = queryString();
+    const {employeecode} = query;
+    Api.login({userId: employeecode}).then((res) => {
+      const data = res.data.dataMap;
+      const {userInfo} = data;
+      LocalStore.set('user', userInfo);
+    });
+  },
+  watch: {
+    $route() {
+      new Jump(store, router).init().then(() => {
+        this.canRender = true;
       });
-    },
-    watch: {
-      $route() {
-        new Jump(store, router).init().then(() => {
-          this.canRender = true;
-        });
-      }
-    },
-    render(h) {
-      if (this.canRender) {
-        return h(App);
-      }
-      return '<div></div>';
     }
-  });
+  },
+  render(h) {
+    if (this.canRender) {
+      return h(App);
+    }
+    return '<div></div>';
+  }
 });
+
