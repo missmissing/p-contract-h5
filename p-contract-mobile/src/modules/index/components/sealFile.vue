7ed726308f3ca2d4fe4a8e95dba52fa76009c125
bizhou.liu <bizhou.liu@chinaredstar.com>
2018-03-02 13:43:15 +0800

    bug修改

--- p-contract-mobile/src/modules/index/components/sealFile.vue
+++ p-contract-mobile/src/modules/index/components/sealFile.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <el-table :data="items">
-      <el-table-column label="文件名" prop="sealFileName">
+      <el-table-column label="文件名" prop="fileName">
         <template scope="scope">
-          <a class="router-link" :href="scope.row.sealFileUrl" target="_blank">{{scope.row.sealFileName}}</a>
+          <a class="router-link" :href="scope.row.fileUrl" target="_blank">{{scope.row.fileName}}</a>
         </template>
       </el-table-column>
-      <el-table-column label="上传人" prop="sealFileCreatorName" width="130"></el-table-column>
-      <el-table-column label="上传时间" prop="sealFileCreateTime" width="130">
+      <el-table-column label="上传人" prop="operatorName" width="130"></el-table-column>
+      <el-table-column label="上传时间" prop="createTime" width="130">
         <template scope="scope">
-          {{scope.row.sealFileCreateTime | formatDate}}
+          {{scope.row.createTime | formatDate}}
         </template>
       </el-table-column>
     </el-table>
