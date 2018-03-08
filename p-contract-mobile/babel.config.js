2997fc8f99c6853f0a6d5be4bb147bc1e90fc389
bizhou.liu <bizhou.liu@chinaredstar.com>
2018-03-08 18:27:33 +0800

    移动端测试

--- p-contract-mobile/babel.config.js
+++ p-contract-mobile/babel.config.js
@@ -16,3 +16,44 @@ module.exports = (config) => {
   return config;
 };
 
+// module.exports = () => {
+//   const config = {
+//     babelrc: false,
+//     compact: false,
+//     cacheDirectory: true,
+//     presets: [
+//       [
+//         'env',
+//         {
+//           modules: false,
+//           targets: {
+//             browsers: [
+//               '> 1%',
+//               'last 2 versions',
+//               'Firefox ESR',
+//               'ie >= 9',
+//               'iOS >= 7',
+//               'Android >= 4.1'
+//             ]
+//           },
+//           useBuiltIns: 'usage'
+//         }
+//       ]
+//     ],
+//     plugins: ['transform-object-rest-spread', ['component',
+//       [
+//         {
+//           libraryName: 'element-ui',
+//           styleLibraryName: 'theme-chalk'
+//         },
+//         {
+//           libraryName: 'mint-ui',
+//           style: true
+//         }
+//       ]
+//     ], 'syntax-dynamic-import']
+//   };
+//
+//   return config;
+// };
+
